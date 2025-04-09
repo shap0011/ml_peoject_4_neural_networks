9c38c06792fce69dcbbce01be922baaa8050ad81
shap0011 <shap0011@algonquinlive.com>
2025-04-09 13:22:45 -0400

    adds necessary packages

---
+++ app_module/functions.py
@@ -0,0 +1,26 @@
+# Loading all the necessary packages
+
+import os
+os.environ['STREAMLIT_LOG_LEVEL'] = 'debug'
+
+import logging
+
+# Configure logging manually again
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+#import numpy as np for numerical computing
+import numpy as np
+# import pandas for data manipulation
+import pandas as pd
+# import matplotlib library for creating plots
+import matplotlib.pyplot as plt
+# import seaborn library for data visualization
+import seaborn as sns
+# import warnings library to manage warning messages
+import warnings
+
+import warnings
+warnings.filterwarnings("ignore")
--- neural_app.py
+++ neural_app.py
@@ -1,5 +1,27 @@
-import streamlit as st
+# Loading all the necessary packages
 
-st.title('🎈 App Name')
+import os
+os.environ['STREAMLIT_LOG_LEVEL'] = 'debug'
 
-st.write('Hello world!')
+import logging
+
+# Configure logging manually again
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+#import numpy as np for numerical computing
+import numpy as np
+# import pandas for data manipulation
+import pandas as pd
+# import matplotlib library for creating plots
+import matplotlib.pyplot as plt
+# import seaborn library for data visualization
+import seaborn as sns
+# import warnings library to manage warning messages
+import warnings
+# import functions from app_module
+from app_module import functions as func
+
+warnings.filterwarnings("ignore")
