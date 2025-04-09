775462193856e8ae1f8fd32ba207e6d82b14e206
shap0011 <shap0011@algonquinlive.com>
2025-04-09 13:37:14 -0400

    adds 'try... except' block, data load

--- neural_app.py
+++ neural_app.py
@@ -11,6 +11,8 @@ logging.basicConfig(
     format="%(asctime)s - %(levelname)s - %(message)s"
 )
 
+# import Streamlit for building web app
+import streamlit as st
 #import numpy as np for numerical computing
 import numpy as np
 # import pandas for data manipulation
@@ -25,3 +27,25 @@ import warnings
 from app_module import functions as func
 
 warnings.filterwarnings("ignore")
+
+try:
+    # Set page configuration
+    st.set_page_config(page_title="Loan Eligibility App", layout="wide")
+
+    # Define color variables
+    header_color = "#c24d2c"  # red color
+    div_color = "#feffe0"  # yellow color
+    subheader_color = "#000"  # yellow color
+
+    # set the title of the Streamlit app
+    st.markdown(f"<h1 style='color: {header_color};'>Project 4. Classification Algorithms</h1>", unsafe_allow_html=True)
+
+    # add subheader
+    st.markdown(f"<h2 style='color: {subheader_color};'>Loan Eligibility Prediction model</h2>", unsafe_allow_html=True)
+
+    # load the dataset from a CSV file located in the 'data' folder
+    df = func.load_data('data/admission.csv')
+    
+except Exception as e:
+    logging.error(f"An error occurred: {e}", exc_info=True)
+    st.error("Something went wrong! Please check the logs or try again later.")
